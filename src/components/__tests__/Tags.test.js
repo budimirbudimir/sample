73ad79d39ca9f2b23931a23c069480b273f4f7ab
budimirbudimir <budimirbudimir@msn.com>
2018-02-23 21:37:11 +0100

    Adding CircleCI initial code

--- src/components/__tests__/Tags.test.js
+++ src/components/__tests__/Tags.test.js
@@ -2,9 +2,15 @@ import React from 'react'
 import Tags from '../Tags'
 import renderer from 'react-test-renderer'
 
-const tags = [{ name: 'rock', url: 'http://budimirx2.com/rock' }]
+const tags = [
+	{ name: 'rock', url: 'https://www.last.fm/tag/rock' },
+	{ name: 'pop', url: 'https://www.last.fm/tag/pop' },
+	{ name: 'metal', url: 'https://www.last.fm/tag/metal' },
+	{ name: 'disco', url: 'https://www.last.fm/tag/disco' },
+	{ name: 'reggae', url: 'https://www.last.fm/tag/reggae' },
+]
 
-it('renders correctly', () => {
+it('Renders without crashing', () => {
 	const tree = renderer.create(<Tags tags={tags} />).toJSON()
 	expect(tree).toMatchSnapshot()
 })
