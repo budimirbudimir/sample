d29424fd86be3804f7623b9dfefc3ed250f54887
budimirbudimir <budimirbudimir@msn.com>
2018-03-02 20:40:10 +0100

    Splitting dev and prod store, adding Redux DevTools

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,6 @@
-import { createStore, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import promise from 'redux-promise-middleware'
-
-import reducers from '../reducers'
-
-// Show logs collapsed (by default) in console
-const logger = createLogger({ collapsed: true })
-
-const middleware =
-	process.env.NODE_ENV !== 'production'
-		? [
-			require('redux-immutable-state-invariant').default(),
-			promise(),
-			thunk,
-			logger,
-		]
-		: [promise(), thunk]
-
-export const store = createStore(reducers, applyMiddleware(...middleware))
+// Determine which store to use, development or production one
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./store.prod')
+} else {
+	module.exports = require('./store.dev')
+}
---
+++ src/store/store.prod.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import promise from 'redux-promise-middleware'
+
+import reducers from '../reducers'
+
+const middleware = [promise(), thunk]
+
+export const store = createStore(reducers, applyMiddleware(...middleware))
